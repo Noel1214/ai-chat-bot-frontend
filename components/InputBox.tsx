1e6f2e0feacaee3846c0e76302db967eb14f4433
Noel Sebu <noelsebu555@gmail.com>
2025-07-08 21:35:23 +0530

    input box completed and made responsive

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,18 +1,86 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import SendIcon from "@mui/icons-material/Send";
+import Button from "@mui/material/Button";
 
 const InputBox = () => {
   return (
-    <Box
-      component="form"
-      sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+    <Box sx={{ position: "relative" }}>
+      <Box
+        sx={{
+          position: "absolute",
+          width: "100vw",
+          bottom: 0,
+          left: 0,
+          paddingX: "1rem",
+          background: "#1E293B",
+          height: "6rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          component="form"
+          autoComplete="on"
+          sx={{
+            width: "96%",
+            height: "50%",
+            // padding: "0.9rem",
+            background: "#334155",
+            display: "flex",
+            alignItems: "center",
+            // justifyContent: "center",
+            borderRadius: 4,
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            placeholder="ask anything!"
+            multiline
+            color="error"
+            sx={{
+              input: {
+                cursor: "pointer",
+                color: "primary",
+                background: "green",
+              },
+              color: "blue",
+              width: "100%",
+              height: "100%",
+              border: "none",
+              "& .MuiOutlinedInput-root": {
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                  outline: "none",
+                },
+                "& .MuiOutlinedInput-input": {
+                  color: "white",
+                },
+                "& fieldset": {
+                  border: "none",
+                },
+              },
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              height: "2.3rem",
+              minWidth: "unset",
+              width: "2.7rem",
+              borderRadius: 3,
+              marginRight: "1rem",
+            }}
+          >
+            <SendIcon
+              sx={{ color: "white", fontSize: 30, padding: 0, margin: 0 }}
+            />
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 };
