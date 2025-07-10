15cdb638afd4dbb60cf9f9c159dc8b8ac5db3be6
Noel Sebu <noelsebu555@gmail.com>
2025-07-10 22:18:29 +0530

    reposone state created and reducer also created

--- lib/features/messages/messagesSlice.ts
+++ lib/features/messages/messagesSlice.ts
@@ -3,17 +3,19 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../store";
 
 interface Message {
-  text: string;
+  response: string;
 }
 
 // Define a type for the slice state
 interface MessagesState {
   messages: Message[];
+  response: string;
 }
 
 // Define the initial state using that type
 const initialState: MessagesState = {
   messages: [],
+  response: "smaple",
 };
 
 export const messagesSlice = createSlice({
@@ -23,12 +25,15 @@ export const messagesSlice = createSlice({
   reducers: {
     // Use the PayloadAction type to declare the contents of `action.payload`
     newMessage: (state, action: PayloadAction<string>) => {
-      state.messages.push({ text: action.payload });
+      state.messages.push({ response: action.payload });
+    },
+    responseSetter: (state, action) => {
+      state.response += action.payload;
     },
   },
 });
 
-export const { newMessage } = messagesSlice.actions;
+  export const { newMessage, responseSetter } = messagesSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectMessages = (state: RootState) => state.messages.messages;
